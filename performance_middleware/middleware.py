45fcd016e32e61eeebbbe7676c8995d3d8286386
Patrick Michaud <pmichaud@uw.edu>
2013-09-05 16:47:52 -0500

    Adding profile data, with configurable thresholds, and sending things to logger w/ thresholds

--- performance_middleware/middleware.py
+++ performance_middleware/middleware.py
@@ -1,19 +1,85 @@
 
-from threading import currentThread
 from datetime import datetime
+from django.conf import settings
+from django.db import connection
+import pstats
+from cStringIO import StringIO
+from random import random
+import logging
+
+try:
+    import cProfile as profile
+except ImportError:
+    import profile
 
 class PerformanceMiddleware(object):
     _process_data = {}
-    def process_request(self, request):
-        thread_id = currentThread()
-        _process_data[thread_id]['start_time'] = datetime.now()
+    profiling = False
+    logger = logging.getLogger(__name__)
 
-    def process_response(self, response):
+    def process_view(self, request, callback, callback_args, callback_kwargs):
+        # self is reused :(
+        self._process_data = {}
+        self.profiling = False
+        self.profiler = None
+        self._process_data['start_time'] = datetime.now()
+
+        profile_per = getattr(settings, "PERFORMANCE_MIDDLEWARE_PROFILE_EVERY", 10)
+
+        random_less_than = 1.0 / profile_per
+
+        rand_val = random()
+
+        if rand_val < random_less_than:
+            self.profiling = True
+            self.profiler = profile.Profile()
+            args = (request,) + callback_args
+            try:
+                return self.profiler.runcall(callback, *args, **callback_kwargs)
+            except:
+                # we want the process_exception middleware to fire
+                # https://code.djangoproject.com/ticket/12250
+                return
+
+
+    def process_response(self, request, response):
         now = datetime.now()
-        thread_id = currentThread()
 
-        start = _process_data[thread_id]['start_time']
-        microseconds = (now - start).microseconds
+        start = self._process_data['start_time']
+        seconds_taken = (now - start).total_seconds()
 
-        print "MS: %i" % (microseconds)
+        warning_threshold = getattr(settings, "PERFORMANCE_MIDDLEWARE_WARNING_THRESHOLD", 1.0)
+        error_threshold = getattr(settings, "PERFORMANCE_MIDDLEWARE_ERROR_THRESHOLD", 2.0)
+        critical_threshold = getattr(settings, "PERFORMANCE_MIDDLEWARE_CRITICAL_THRESHOLD", 5.0)
+
+        if (seconds_taken < warning_threshold) and (seconds_taken < error_threshold) and (seconds_taken < critical_threshold):
+            return response
+
+        io = StringIO()
+        io.write("Time taken: %f seconds\n" % seconds_taken)
+        io.write("Request: \n%s\n" % request.__str__())
+        io.write("Profile: \n")
+
+        if self.profiling:
+            self.profiler.create_stats()
+            stats = pstats.Stats(self.profiler, stream=io)
+            stats.sort_stats('cumulative')
+            stats.print_stats(100)
+        else:
+            io.write("No profile for this request, sorry")
+
+        io.write("SQL:\n")
+
+        for query in connection.queries:
+            io.write("Time: %s, Query: %s\n" % (query['time'], query['sql']))
+
+        if seconds_taken > critical_threshold:
+            self.logger.critical(io.getvalue())
+
+        elif seconds_taken > error_threshold:
+            self.logger.error(io.getvalue())
+
+        elif seconds_taken > warning_threshold:
+            self.logger.warning(io.getvalue())
+
         return response
