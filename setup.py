638f0114979a0d61f7dec291555fb8c80e5c2519
Patrick Michaud <pmichaud@uw.edu>
2013-09-05 14:48:03 -0500

    Adding a setup.py so i can install with pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Django-Performance-Middleware',
+      version='1.0',
+      description='A tool to track down slowness in your django app',
+      install_requires=['Django==1.4'],
+     )
